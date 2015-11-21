25c93ac9ba959a2a29c563f0a3c102015d5468d1
Joshua Chan <jchan.is@gmail.com>
2015-11-21 14:52:31 -0500

    MongoDB model schemas and question/advice templates

---
+++ models/Patient.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+
+var patientSchema = new mongoose.Schema({
+  id: { type: String, default: '' },
+  phone: { type: String, unique: true, lowercase: true },
+  name: { type: String, default: '' },
+  gender: { type: String, default: '' },
+  location: { type: String, default: '' },
+  dob: { type: String, default: '' },
+  pregnancy_date: { type: String, default: '' },
+  child_dob: { type: String, default: '' },
+  clinic: { type: String, default: '' },
+  history: [{ type: Schema.Types.Mixed }]
+});
+
+module.exports = mongoose.model('patient', patientSchema);
+
+
+/*
+  history:
+
+  {
+    issue: "headache".
+    date: "2014-02-20",
+    resolved: true,
+    alerted: false
+  }
+
+ */
